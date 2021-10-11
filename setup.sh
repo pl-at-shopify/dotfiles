9a38303c3bbc75ffcb3c34c9290e07c3b9fd3c95
Peter Leonov <peter.leonov@shopify.com>
2021-10-11 20:05:36 +0200

    spin + sorbet

--- setup.sh
+++ setup.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-# testing dagerous stuff
-curl -sS https://up.dev | sh
+if [ $SPIN ]; then
+    # Install packages
+    sudo apt install -y nano mc
+
+    # Add git aliases
+    git config --global alias.co checkout
+    git config --global alias.br branch
+    git config --global alias.st status
+    git config --global alias.ci commit
+    git config --global alias.hr "!git reset --hard && git clean -f -d"
+
+    # Link dotfiles
+    ln -sf ~/dotfiles/zshrc ~/.zshrc
+fi
